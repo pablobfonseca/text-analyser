538f6fd9516ed37b97b60b5024ebb7e2b54c6201
Pablo Fonseca <pablofonseca777@gmail.com>
2016-08-25 01:15:42 -0300

    Add good words counter

--- lib/text_analyser.rb
+++ lib/text_analyser.rb
@@ -1,4 +1,6 @@
 class TextAnalyser
+  STOP_WORDS = %w{the a by on for of are with just but and to the my I has some in}.freeze
+
   attr_reader :file_lines, :text
 
   def initialize(file_lines)
@@ -30,12 +32,21 @@ class TextAnalyser
     text.split(/\n\n/).length
   end
 
+  def count_good_words
+    good_words = all_words.reject { |word| STOP_WORDS.include?(word) }
+    good_words.length
+  end
+
   def sentence_per_paragraph
     count_sentence / count_paragraphs
   end
 
   def words_per_sentence
     count_words / count_sentence
+  end
+
+  def good_words_per_sentence
+    ((count_good_words.to_f / all_words.length.to_f) * 100).to_i
   end
 
   def report
@@ -47,11 +58,19 @@ class TextAnalyser
       #{count_characters} characters
       #{count_nonspace_characters} characters excluding spaces
       #{count_words} words
+      #{count_good_words} good words (without stop words)
       #{count_paragraphs} paragraphs
       #{count_sentence} sentences
       #{sentence_per_paragraph} sentences per paragraph (average)
       #{words_per_sentence} words per sentence (average)
+      #{good_words_per_sentence}% of the words are non-fluff words
       #{'=' * 30}
     TEXT
+  end
+
+  private
+
+  def all_words
+    text.scan(/\w+/)
   end
 end
--- test/text_analyser_test.rb
+++ test/text_analyser_test.rb
@@ -32,12 +32,20 @@ class TextAnalyserTest < Minitest::Test
     assert_equal 20, @analyser.count_paragraphs
   end
 
+  def test_count_good_words
+    assert_equal 865, @analyser.count_good_words
+  end
+
   def test_sentence_per_paragraph
     assert_equal 2, @analyser.sentence_per_paragraph
   end
 
   def test_words_per_sentence
     assert_equal 24, @analyser.words_per_sentence
+  end
+
+  def test_good_words_per_sentence
+    assert_equal 76, @analyser.good_words_per_sentence
   end
 
   def test_report
@@ -49,10 +57,12 @@ class TextAnalyserTest < Minitest::Test
     6376 characters
     5140 characters excluding spaces
     1111 words
+    865 good words (without stop words)
     20 paragraphs
     45 sentences
     2 sentences per paragraph (average)
     24 words per sentence (average)
+    76% of the words are non-fluff words
     ==============================
     TEXT
     assert_equal expected_text, @analyser.report
